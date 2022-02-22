4b848221969ab34f8ca87cbf2dd7a6dea9ac71d4
Jesper Hagman <Jesper.hagman98@gmail.com>
2022-02-22 12:00:08 +0100

    adding counter map

---
+++ counter/src/components/countComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class countComponent extends Component {
+  render() {
+    return (
+      <div>countComponent</div>
+    )
+  }
+}
+
+export default countComponent
